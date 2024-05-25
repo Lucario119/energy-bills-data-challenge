6bce8e0ef551622fe713c1b706f11896488cb8a6
LucasOliveira119 <lucasoliveira@proccedure.com>
2024-05-24 22:51:36 -0400

    ajustes render

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -3,10 +3,7 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-const env = process.env.NODE_ENV || 'development';
-
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL!,{
   dialect: 'postgres',
 });
 
